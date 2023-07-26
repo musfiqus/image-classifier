70bb8627af74cc78e4611b99819714708fec52e1
Mushfiqus Salehin <musfiqus@gmail.com>
2023-07-26 04:22:27 -0230

    add: percentage bars for results

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,7 +4,7 @@ import ImageClassifier from "@/components/ImageClassifier";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-200">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
       <h1 className="text-3xl font-semibold mb-8">Image Classifier</h1>
       <ImageClassifier />
     </main>
