df9076334147a832b40b387df03d181000056609
Mushfiqus Salehin <musfiqus@gmail.com>
2024-01-15 01:25:11 -0330

    Add github actions deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DEBUG_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY') or os.getenv('DEBUG_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -144,6 +144,7 @@ DEFAULT_LABELS_URL = os.getenv('DEFAULT_LABELS_URL')
 
 CORS_ALLOWED_ORIGINS = [
     os.getenv('DEBUG_FRONTEND_URL'),
+    os.getenv('FRONTEND_URL')
 ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
